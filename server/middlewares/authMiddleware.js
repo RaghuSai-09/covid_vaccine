782990218bc9c3384a9584d4f2a25f86aa33fd11
raghusais <raghusai_kosana@srmap.edu.in>
2023-06-29 02:30:09 +0530

    Changes made

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ exports.isAdmin = (req, res, next) => {
   try {
    
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'secretKey');
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     if (!decodedToken.isAdmin) {
       return res.status(401).json({ message: 'Admin access denied' });
     }
