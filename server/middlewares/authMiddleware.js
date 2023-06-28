c5b1b38aeb816c747259c8bab313a0587f5655f3
Raghu Sai K <89162131+RaghuSai-09@users.noreply.github.com>
2023-06-29 02:31:59 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 exports.isAdmin = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'secretKey');
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decodedToken.isAdmin) {
       return res.status(401).json({ message: 'Admin access denied' });
